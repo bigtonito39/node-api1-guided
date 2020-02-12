d831af855e15fee1ad7df9e0d51f3879cb1f0446
Reinoso <jreinoso@amazon.com>
2020-02-12 07:39:51 -0500

    guided project better organized, and working better

--- server.js
+++ server.js
@@ -1,120 +1,98 @@
-/* //RAW Server Starts here
-
-const http = require("http")
-const server = http.createServer((req, res) => {
-
-	// a "successful" status code
-
-	res.statusCode = 200
-	// return some JSON to the client
-	res.setHeader("Content-Type", "application/json")
-	res.write(`{"message": "hello, world"}`)
-	// send the response off
-	res.end()
-
-})
-
-​
-
-server.listen(8080, () => {
-	console.log(`server started at http://localhost:8080`)
-})
-
-*/ //RAW Server ends here
-
-//using express starts here
+//Server using express framework
 
 const express = require("express")
-//make sure to have users as let, since we are mutating it.
-let users =require("./users")
+let users = require("./users")
+
 const server = express()
 
 // this is middleware that allows express
-//to parse JSON request bodies. We'll talk about this more later.
-server.use(express.json)
+// to parse JSON request bodies. We'll talk about this more later.
+server.use(express.json())
 
 server.get("/", (req, res) => {
-res.json({message:"hello, world"})
-
+	res.json({ message: "hello, world" })
 })
 
-server.get("/lambda", (req,res)=> {
-    res.redirect("https://lambdaschool.com")
+server.get("/lambda", (req, res) => {
+	// this will automatically return all the required response headers
+	// and status code for a proper HTTP redirect
+	res.redirect("https://lambdaschool.com")
 })
-//simulating a real database call
+
 server.get("/users", (req, res) => {
-    res.json(users)
+	// this is simulating a real database call
+	res.json(users)
 })
 
 server.get("/users/:id", (req, res) => {
-    //pull the ID value from the url
-    const id = req.params.id;
-    //find the specific user from our fake databas with that ID
-    const user = users.find(u=> u.id ==id)
+	// pull the ID value from the URL
+	const id = req.params.id
+	// find the specific user from our fake database with that ID
+	const user = users.find(u => u.id == id)
 
-    //a user was found with that ID
-
-    if(user) {
-        //RETURN THE DATA TO THE CLIENT
-        res.json(user)
-    }else {
-        //RETURN AN ERROR TO THE CLIENT
-        res.status(404).json({message:"User not found"})
-    }
+	// a user was found with that ID
+	if (user) {
+		// return the data to the client
+		res.json(user)
+	// no user was found with that ID
+	} else {
+		// return an error to the client
+		res.status(404).json({ message: "User not found" })
+	}
 })
 
 server.post("/users", (req, res) => {
-    //create a new fake user
-    const newUser = {
-        id:users.length + 1,
-        name: req.body.name
-    }
-    //simulate the action of "inserting " to our database, 
-    // we are just using to keep it simple, we wont use push in real world data base.
-    users.push(newUser)
-    //201 means success and a resource was created
-    res.status(201).json(newUser)
-})
+	// create a new fake user
+	const newUser = {
+		id: users.length + 1,
+		// the request body is sent by the client,
+		// in this case a JSON object that we can read from
+		name: req.body.name,
+	}
 
-server.delete("/users:id", (req, res) => {
-  //find the specific user from our fake database with that ID
-  const user = users.find(u => u.id == req.params.id)
-
-  //user exist in the database
-  if (user) {
-                        //check thi  u.id !== req.params.id it might help for mimic site work at my job    
-      users = users.filter(u => u.id !== req.params.id)
-// a sucessful response with no response body
-     res.status(204).end()
-
-      //user does not exist in the database
-  } else {
-      res.status(404).json({message:"User not Found"})
-      //return an error
-  }
-
+	// simulate the action of "inserting" to our database
+	users.push(newUser)
+	// 201 means success and a resource was created
+	res.status(201).json(newUser)
 })
 
 server.put("/users/:id", (req, res) => {
-    //finds the location of the user we are updating in the fake database
-    const index = users.findIndex(  u=> u.id == req.params.id)
-//update that users name if a new value is sent in the request body
-    if (req.body.name) {
-        users[index].name = req.body.name
-    }
-// return the updated user data
-    res.json(users[index])
+	// finds the location of the user we're updating in the fake database
+	const index = users.findIndex(u => u.id == req.params.id)
+	// return a 404 if the user with that ID doesn't exist
+	if (index === -1) {
+		return res.status(404).json({ message: "User not found" })
+	}
+
+	// update that user's name if a new value is sent in the request body
+	if (req.body.name) {
+		users[index].name = req.body.name
+	}
+
+	// return the updated user data
+	res.json(users[index])
 })
 
+server.delete("/users/:id", (req, res) => {
+	// find the specific user from our fake database with that ID
+	const user = users.find(u => u.id == req.params.id)
 
+	// user exists in the database
+	if (user) {
+		users = users.filter(u => u.id != req.params.id)
+		// a successful response with no response body
+		res.status(204).end()
+	// user does not exist in the database
+	} else {
+		res.status(404).json({ message: "User not found" })
+	}
+})
 
-//start the server on localhost at port 8080
 const port = 8080
+
+// start the server on localhost at port 8080
 server.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`)
+	console.log(`server started at http://localhost:${port}`)
 })
-//using express ends here
-
-
 
 
